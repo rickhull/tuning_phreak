983e639a1fa839723a44995e1b1182561505807a
Rick Hull <rick.hull@gmail.com>
2023-12-30 18:18:29 -0500

    add more successful parse cases

--- test/tuning_phreak.rb
+++ test/tuning_phreak.rb
@@ -6,7 +6,7 @@ describe TuningPhreak do
     expect(TuningPhreak::A4).must_be_kind_of Numeric
   end
 
-  it "defaults to 440 Hz" do
+  it "defaults to A4 = 440 Hz" do
     expect(TuningPhreak::A4).must_equal 440
   end
 
@@ -14,6 +14,14 @@ describe TuningPhreak do
     note_sym, octave_num = TuningPhreak.parse('C#3')
     expect(note_sym).must_equal :c_sharp
     expect(octave_num).must_equal 3
+
+    note_sym, octave_num = TuningPhreak.parse('Bb7')
+    expect(note_sym).must_equal :b_flat
+    expect(octave_num).must_equal 7
+
+    note_sym, octave_num = TuningPhreak.parse('C0')
+    expect(note_sym).must_equal :c
+    expect(octave_num).must_equal 0
   end
 
   it "rejects note strings like: bb4 or Bf4" do
