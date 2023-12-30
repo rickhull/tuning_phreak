1815d0bf9a4d4651dc7eca1e7a346a809b37db6c
Rick Hull <rick.hull@gmail.com>
2023-12-30 16:53:58 -0500

    update tests; add rake test

---
+++ test/diatonic.rb
@@ -0,0 +1,23 @@
+require 'tuning_phreak/diatonic'
+require 'minitest/autorun'
+
+include TuningPhreak
+
+describe Diatonic do
+  [:c_major, :a_minor].each { |scale|
+    it "uses whole number ratios to describe perfect intervals" do
+    end
+
+    it "can determine the frequency of any white key on a piano" do
+    end
+
+    it "accepts any value for the frequency of A4" do
+    end
+  }
+
+  describe "C Major" do
+  end
+
+  describe "A Minor" do
+  end
+end
--- test/temperament.rb
+++ test/temperament.rb
@@ -4,13 +4,47 @@ require 'minitest/autorun'
 include TuningPhreak
 
 describe Temperament do
-  it "converts musical notes to audio frequencies" do
-    t = Temperament.new
-    expect(t.freq('A4')).must_equal 440.0
+  [nil, :equal].each { |name|
+    it "converts musical notes to audio frequencies" do
+      t = Temperament.new(name: name)
+      a4 = t.freq('A4')
+      b4 = t.freq('B4')
 
-    t = Temperament.new(a4: 442)
-    expect(t.freq('A4')).must_equal 442.0
-    expect(t.freq('A5')).must_equal 884.0
-    expect(t.freq('A3')).must_equal 221.0
+      expect(a4).must_equal 440.0
+      expect(b4).must_be :>, a4
+    end
+
+    it "allows any frequency for A4" do
+      t = Temperament.new(a4: 442, name: name)
+      expect(t.freq('A4')).must_equal 442.0
+      expect(t.freq('A5')).must_equal 884.0
+      expect(t.freq('A3')).must_equal 221.0
+    end
+
+    it "respects octave doubling" do
+      t = Temperament.new(name: name)
+      c3 = t.freq('C3')
+      c4 = t.freq('C4')
+      c5 = t.freq('C5')
+
+      expect(c4).must_equal c3 * 2
+      expect(c5).must_equal c4 * 2
+    end
+  }
+
+  describe "the 4/5 comma system" do
+    it "has chromatic semitones at 5 commas wide" do
+    end
+
+    it "has diatonic semitones at 4 commas wide" do
+    end
+
+    it "has 53 commas per octave" do
+    end
+  end
+
+  describe "equal temperament" do
+    it "has 12 semitones of equal width" do
+    end
   end
 end
